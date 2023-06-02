1c10367f76700dd89c86b5b5c4bdb1585cdef181
AAAnDreeYauri <129539190+AAAndReeY@users.noreply.github.com>
2023-06-02 14:21:18 -0500

    Add files via upload

---
+++ src/t3tecpocatalogo/Catalogo.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t3tecpocatalogo;
+
+/**
+ *
+ * @author Personal
+ */
+public class Catalogo {
+    
+    private String NombreRopas;
+    private String PrecioRopas;
+    private String CodRopa;
+
+    public String getNombreRopas() {
+        return NombreRopas;
+    }
+
+    public void setNombreRopas(String NombreRopas) {
+        this.NombreRopas = NombreRopas;
+    }
+
+    public String getPrecioRopas() {
+        return PrecioRopas;
+    }
+
+    public void setPrecioRopas(String PrecioRopas) {
+        this.PrecioRopas = PrecioRopas;
+    }
+
+    public String getCodRopa() {
+        return CodRopa;
+    }
+
+    public void setCodRopa(String CodRopa) {
+        this.CodRopa = CodRopa;
+    }
+    
+   
+    
+    
+}
